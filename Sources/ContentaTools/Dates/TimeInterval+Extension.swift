9dea20c1c1842eaa200f0a68319a87d0019c62c9
Vito Librarius <vitolibrarius@gmail.com>
2018-09-12 12:39:00 -0600

    Forgot to make the extensions public

--- Sources/ContentaTools/Dates/TimeInterval+Extension.swift
+++ Sources/ContentaTools/Dates/TimeInterval+Extension.swift
@@ -5,11 +5,11 @@
 import Foundation
 
 extension TimeInterval {
-    static var second: TimeInterval { return 1           }
-    static var minute: TimeInterval { return second * 60 }
-    static var hour: TimeInterval   { return minute * 60 }
-    static var day: TimeInterval    { return hour * 24   }
-    static var week: TimeInterval   { return day * 7     }
-    static var month: TimeInterval  { return day * 30    }
-    static var year: TimeInterval   { return day * 365   }
+    public static var second: TimeInterval { return 1           }
+    public static var minute: TimeInterval { return second * 60 }
+    public static var hour: TimeInterval   { return minute * 60 }
+    public static var day: TimeInterval    { return hour * 24   }
+    public static var week: TimeInterval   { return day * 7     }
+    public static var month: TimeInterval  { return day * 30    }
+    public static var year: TimeInterval   { return day * 365   }
 }
--- Sources/ContentaTools/Strings/String+Extensions.swift
+++ Sources/ContentaTools/Strings/String+Extensions.swift
@@ -5,38 +5,38 @@
 import Foundation
 
 extension String {
-    func removeCharacterSet(from forbiddenChars: CharacterSet) -> String {
+    public func removeCharacterSet(from forbiddenChars: CharacterSet) -> String {
         let passed = self.unicodeScalars.filter { !forbiddenChars.contains($0) }
         return String(String.UnicodeScalarView(passed))
     }
     
-    func removeCharacters(from: String) -> String {
+    public func removeCharacters(from: String) -> String {
         return removeCharacterSet(from: CharacterSet(charactersIn: from))
     }
 
-    func removeCharactersNotIn(from: String) -> String {
+    public func removeCharactersNotIn(from: String) -> String {
         return removeCharacterSet(from: CharacterSet(charactersIn: from).inverted)
     }
 
     // replaces multiple spaces in string with single space
-    func condensingWhitespace() -> String {
+    public func condensingWhitespace() -> String {
         return self.components(separatedBy: .whitespacesAndNewlines)
             .filter { !$0.isEmpty }
             .joined(separator: " ")
     }
 
     // Remove whitespace and newlines
-    mutating func trim() {
+    mutating public func trim() {
         self = trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
     }
 
-    mutating func replaceOccurrencesOfStringsWithString(_ strings: [String], _ replacementString: String) {
+    mutating public func replaceOccurrencesOfStringsWithString(_ strings: [String], _ replacementString: String) {
         for string in strings {
             self = replacingOccurrences(of: string, with: replacementString)
         }
     }
 
-    func isStringIn(array: [String], ignoreCase: Bool = true) -> Bool {
+    public func isStringIn(array: [String], ignoreCase: Bool = true) -> Bool {
         for string in array {
             if ignoreCase == true && string.caseInsensitiveCompare(self) == .orderedSame {
                 return true
